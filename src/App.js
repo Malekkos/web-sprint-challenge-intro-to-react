d1051c10015cf38868e82a6ab0ce63ef423477c9
Christian Anderson <tehninjawaffle@gmail.com>
2023-07-14 08:32:14 -0700

    Imported effect and state, declared state variables initialized to null, started the useEffect hook and got the characters from the link. Setting those characters to our state]

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
-
+import React, {useEffect, useState} from 'react';
+import axios from "axios"
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+const [characters, setCharacters] = useState(null)
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+useEffect(() => {
+  axios.get("https:swapI.dev/api/people/")
+  .then(res => {
+    console.log(res);
+    setCharacters(res.data)
+  })
+}, [])
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
